10b1ab751b32c018fdd99a4e4ffc14c9f88f092c
mehedifiz <mehedi.shanto.contract@gamil.com>
2024-06-06 23:28:34 +0600

    left side nevigation

--- src/Pages/Shared/Leftnsv.jsx
+++ src/Pages/Shared/Leftnsv.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 
 const Leftnsv = () => {
+
+    const [data , setdata] =useState([])
+
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data  =>setdata(data))
+    },[])
     return (
         <div>
-            <h2>Left nav </h2>
+
+            <h2 className="pl-5 py-5 font-semibold  ">All Caterogy</h2>
+            <h2 className="pl-5 py-5 bg-slate-400 text-black text-2xl font-semibold w-ful text-center">National News</h2>
+
+                {
+                    data.map(data => <li key={data.id} className="list-none text-center py-5 text-gray-300">{data.name}</li>)
+                }
+
+           
         </div>
     );
 };
--- src/Pages/Shared/Rightnav.jsx
+++ src/Pages/Shared/Rightnav.jsx
@@ -1,8 +1,35 @@
+import { FaFacebook, FaGithub, FaGoogle, FaTwitter } from "react-icons/fa";
+
+import qz1 from '../../assets/qZone1.png'
+import qz2 from '../../assets/qZone2.png'
+import qz3 from '../../assets/qZone3.png'
 
 const Rightnav = () => {
     return (
         <div>
-            <h2>Right side nav</h2>
+          <div className="p-4  mb-6">
+            <h2 className="font-semibold  ">Login With </h2>
+            <button className="btn btn-outline w-full mb-4">
+            <FaGoogle />
+Google</button>
+
+<button className="btn btn-outline w-full">
+            <FaGithub />
+Github</button>
+
+          </div>
+
+          <div className="p-4  mb-6">
+            <h2 className="font-semibold ">Q Zone</h2>
+
+            <img src={qz1} alt="" />
+            <img src={qz2} alt="" />
+            <img src={qz3} alt="" />
+            
+
+          </div>
+
+          
         </div>
     );
 };
